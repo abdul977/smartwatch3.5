1cd412ebf4862a2600b1e49a5f1c251972e14bdf
abdul977 <47270099+abdul977@users.noreply.github.com>
2024-12-03 08:47:19 +0100

    Add project/src/utils/nigerianNames.ts

---
+++ project/src/utils/nigerianNames.ts
@@ -0,0 +1,6 @@
+export const nigerianNames = [
+  'Oluwaseun', 'Chioma', 'Babajide', 'Adebayo', 'Folake',
+  'Olayinka', 'Chinua', 'Ngozi', 'Obioma', 'Tunde',
+  'Aisha', 'Chidi', 'Folami', 'Kehinde', 'Olayinka',
+  'Bamidele', 'Chibueze', 'Oluwafemi', 'Adanna', 'Yetunde'
+];
